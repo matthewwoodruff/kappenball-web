341a7de5343248892fd386be1283eb0628ba186f
Matt Woodruff <mwoodruf@thoughtworks.com>
2016-07-07 19:07:45 +0100

    Matt - Adds a tag for every successful build in CI

--- scripts/package.sh
+++ scripts/package.sh
@@ -10,3 +10,7 @@ tar -vczf dist/kappenball.tar.gz \
     --exclude=".DS_Store" \
     --exclude="dist" \
     .
+if ! [[ -z $SNAP_PIPELINE_COUNTER ]]; then
+git tag build-$SNAP_PIPELINE_COUNTER 
+git push origin --tags
+fi
